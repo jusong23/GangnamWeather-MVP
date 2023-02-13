3478fad7d7d16878503f93b7ad05ce9a7488ec06
jusong23 <jusong23@naver.com>
2023-02-13 15:32:51 +0900

    MVP Refatctoring

---
+++ GangnamWeather+MVVM/MVP/TempViewContract.swift
@@ -0,0 +1,8 @@
+//
+//  TempViewContract.swift
+//  GangnamWeather+MVVM
+//
+//  Created by iot-parksooseong on 2023/02/13.
+//
+
+import Foundation
---
+++ GangnamWeather+MVVM/MVP/TempViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TempViewController.swift
+//  GangnamWeather+MVVM
+//
+//  Created by iot-parksooseong on 2023/02/13.
+//
+
+import Foundation
